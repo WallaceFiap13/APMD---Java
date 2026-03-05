bd3285058871597c0cf5d805d0e6a288a4a53b4a
WallaceQ13 <wallace.lima6@etec.sp.gov.br>
2026-03-04 22:36:37 -0300

    Aula 2 - 04/03/2026

--- Semestre_1/src/Aula_2/Exemplo_1.java
+++ Semestre_1/src/Aula_2/Exemplo_1.java
@@ -2,6 +2,25 @@ package Aula_2;
 
 public class Exemplo_1 {
     public static void main(String[] args) {
-        System.out.println("Aula 2, Exemplo 1");
+        double x, y, z, w, M, Mn; // Declarando as variáveis x,y,z,w como reais
+        int m, n = 1; // Declarando m, n como inteiros e atribuindo valor a n
+        x = 3.2;
+        y = x * 2;
+        z = (x + y)/ 2;
+        w = x + y/ 2;
+        m = n/2; // Inteiro dividido por inteiro resulta em inteiro, não pega as casas decimais
+        M = n/2; // Mesmo colocando a variável que vai armazenar como real, a conta ainda gera só inteiro
+        Mn = (double) n/2; // Agora a gente considera n como real e a conta resulta em real, guardando em Mn que é real
+
+
+        System.out.println("");
+        System.out.println("Valor dado de X = " + x);
+        System.out.println("Valor calculado de Y = " + y);
+        System.out.println("Valor calculado de Z = " + z);
+        System.out.println("Valor calculado de W = " + w);
+        System.out.println("Valor calculado de M = " + m);
+        System.out.println("Valor calculado de M = " + M);
+        System.out.println("Valor calculado de M = " + Mn);
+        System.out.println("");
     }
 }
---
+++ Semestre_1/src/Aula_2/Exercicio_1.java
@@ -0,0 +1,15 @@
+package Aula_2;
+
+public class Exercicio_1 {
+    public static void main(String[] args) throws Exception {
+        double preco, desconto10;
+
+        preco = 23.5;
+        desconto10 = preco * 0.9;
+
+        System.out.printf("%s%.2f\n","O preço com desconto é: R$ ", desconto10);
+        // %s - indica que vai vir uma frase em primeiro do print
+        // %.2f - formata em duas casas
+        // \n para pular linha
+    }
+}
