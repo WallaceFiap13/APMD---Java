bf1b8f271afb830436af187dbac9f6924674b32c
WallaceQ13 <wallace.lima6@etec.sp.gov.br>
2026-03-19 22:19:41 -0300

    Correção do exercício 7

--- Semestre_1/src/Avaliações/Lista_de_Exercícios_1/Exercicios_7.java
+++ Semestre_1/src/Avaliações/Lista_de_Exercícios_1/Exercicios_7.java
@@ -13,29 +13,34 @@ public class Exercicios_7 {
             System.out.print("Insira o seu peso em kg (digite -1 para encerrar o programa): ");
             peso = inputkeyboard.nextDouble();
 
-            System.out.print("Insiraa sua altura em metros: ");
-            altura = inputkeyboard.nextDouble();
+            if(peso != -1){
+                System.out.print("Insiraa sua altura em metros: ");
+                altura = inputkeyboard.nextDouble();
 
-            imc = peso / (altura * altura);
+                imc = peso / (altura * altura);
 
-            if (imc < 18.5) {
-                System.out.println("");
-                System.out.println("Baixo Peso");
+                if (imc < 18.5) {
+                    System.out.println("");
+                    System.out.println("Baixo Peso");
+                }
+                else if (imc >= 18.5 && imc <= 24.9) {
+                    System.out.println("");
+                    System.out.println("Peso Normal");
+                }
+                else if (imc >= 25 && imc <= 29.9) {
+                    System.out.println("");
+                    System.out.println("Sobrepeso");
+                }
+                else if (imc >= 30) {
+                    System.out.println("");
+                    System.out.println("Obeso");
+                }
             }
-            else if (imc >= 18.5 && imc <= 24.9) {
-                System.out.println("");
-                System.out.println("Peso Normal");
-            }
-            else if (imc >= 25 && imc <= 29.9) {
-                System.out.println("");
-                System.out.println("Sobrepeso");
-            }
-            else if (imc >= 30) {
-                System.out.println("");
-                System.out.println("Obeso");
-            }
-
         } while(peso != -1);
+
+        System.out.println("");
+        System.out.println("Sistema encerrado!!");
+        System.out.println("");
 
         inputkeyboard.close();
     }
