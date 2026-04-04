2b67dffe7a6988cf6febb267d8fe53f5a33725ae
WallaceQ13 <wallace.lima6@etec.sp.gov.br>
2026-04-03 21:50:55 -0300

    .

--- Semestre_1/src/Aula_3/Exercícios/Exercicio_5.java
+++ Semestre_1/src/Aula_3/Exercícios/Exercicio_5.java
@@ -4,7 +4,7 @@ public class Exercicio_5 {
     public static void main(String[] args) {
         double C, F;
 
-        System.out.println("Tabela de conversão de Fahrenheit para Celsius de 100°F a 150°F\n");
+        System.out.println("\nTabela de conversão de Fahrenheit para Celsius de 100°F a 150°F\n");
         for (F = 100; F <= 150; F++) {
             C = ((double)5/9)*(F-32);
             System.out.printf("%.1f°F = %.1f°C\n", F, C);
