5bc507f7119d280726127a6ee66407bb92421972
WallaceQ13 <wallace.lima6@etec.sp.gov.br>
2026-04-03 22:00:26 -0300

    Exercício 5 finalizado

--- Semestre_1/src/Aula_3/Exercícios/Exercicio_5.java
+++ Semestre_1/src/Aula_3/Exercícios/Exercicio_5.java
@@ -3,6 +3,16 @@ package Aula_3.Exercícios;
 public class Exercicio_5 {
     public static void main(String[] args) {
         double C, F;
+        // double F_1;
+
+        // do{
+        //     F_1 = 100;
+        //     C = ((double)5/9)*(F_1-32);
+        //     System.out.printf("%.1f°F = %.1f°C\n", F_1, C);
+        //     F_1++;
+        // } while (F_1 <=150);
+
+        // Envés de fazer isso a cima com o while, fazemos a opção abaixo com for
 
         System.out.println("\nTabela de conversão de Fahrenheit para Celsius de 100°F a 150°F\n");
         for (F = 100; F <= 150; F++) {
